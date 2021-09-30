4746eaf17727e2055be37b3ca25d4ccb06f272e1
Lucas <lucasakajiri@hotmail.com>
2021-09-30 11:38:19 -0300

    Aula DIO Trabalhando com Componentes em React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Item from "./components/item";
+import Card from "./components/Card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Bom Dia</h1>
+      <h2>Minha Primeira Aplicacao com React</h2>
+      <ul>
+        <Item> Item 01</Item>
+        <Item> Item 02</Item>
+        <Item> Item 03</Item>
+
+      </ul>
+    <Card/>
+    </>
+
+    
   );
 }
 
